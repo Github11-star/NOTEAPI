32a8bb18e38b759fd845b2dc1be9823c878a066f
Firoz Shaikh <gitfiroz695@gmail.com>
2023-07-03 10:41:40 +0530

    Local API Setup + Nodemon

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hello CheezyCode");
+const express = require("express");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+const quotes = require("./quotes.json")
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send("Hello");
+});
+
+app.get("/quote",(req, res)=>{
+    res.status(200).json(quotes);
+});
+
+app.get("/random", (req, res)=>{
+    let index = Math.floor(Math.random() * quotes.length);
+    let quote = quotes[index];
+    res.status(200).json(quote); 
+});
+
+app.listen(8000, ()=>{
+    console.log("Server started onpost no. 5000");
+});
