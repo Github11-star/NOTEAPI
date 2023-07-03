0f9c833908e2c313c036aae67251762f4b48ca8d
Firoz Shaikh <gitfiroz695@gmail.com>
2023-07-03 16:09:30 +0530

    JWT Token Authentication - Login & Signup

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const req = require("express/lib/request");
 const res = require("express/lib/response");
+const { signup, signin } = require("../controllers/userControllers");
 const userRouter = express.Router();
 
-userRouter.post("/signup", (req, res)=>{
-    res.send("signup");
-});
+userRouter.post("/signup", signup);
 
-userRouter.post("/signin", (req, res)=>{
-    res.send("signin");
-});
+userRouter.post("/signin", signin);
 
 module.exports = userRouter;
 
