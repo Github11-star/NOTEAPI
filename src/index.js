f6394460ccb9c1122814c89da256d91a0df3032e
Firoz Shaikh <gitfiroz695@gmail.com>
2023-07-06 12:29:23 +0530

    remove boiler plate code

--- src/index.js
+++ src/index.js
@@ -4,21 +4,14 @@ const noteRouter = require("./routes/noteRoutes");
 const app = express();
 
 const mongoose = require("mongoose");
-const req = require("express/lib/request");
-const res = require("express/lib/response");
 
 app.use(express.json());
-
-app.use((req, res, next)=>{
-    console.log("HTTP Method - " + req.method + ", URL - " + req.url);
-    next();
-});
 
 app.use("/users", userRouter);
 app.use("/note", noteRouter);
 
 app.get("/", (req, res)=>{
-    res.send("Hello");
+    res.send("Note API from chezzy codes");
 });
 
 mongoose.connect("mongodb+srv://admin:admin@cluster0.5y4negj.mongodb.net/?retryWrites=true&w=majority")
