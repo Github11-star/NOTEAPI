573d6cdf1681a25c6fd1323af4a28e885de00835
Firoz Shaikh <gitfiroz695@gmail.com>
2023-07-07 11:18:47 +0530

    connection string added

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res)=>{
 
 const PORT = process.env.PORT || 8000;
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect("mongodb+srv://admin:admin@cluster0.5y4negj.mongodb.net/notes_db?retryWrites=true&w=majority")
 .then(()=>{
     app.listen(PORT, ()=>{
         console.log("Server started onpost no. " + PORT);
@@ -31,4 +31,3 @@ mongoose.connect(process.env.MONGO_URL)
 .catch((error)=>{
     console.log(error);
 })
-
