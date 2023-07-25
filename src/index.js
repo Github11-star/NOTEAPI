704a419a79e49b5fab5816da2616c23c4012bc44
Firoz Shaikh <gitfiroz695@gmail.com>
2023-07-25 14:45:48 +0530

    local server working for noteApp of android

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.get("/", (req, res)=>{
     res.send("WELCOME FROM CHEZZY CODE");
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 9000;
 
 mongoose.connect("mongodb+srv://admin:admin@cluster0.5y4negj.mongodb.net/notes_db?retryWrites=true&w=majority")
 .then(()=>{
